aabe14116e29c65f4693f93d10a6311e4eccb086
HP <fatimaezzahrabouaziz@gmail.com>
2024-02-25 18:44:52 +0100

    test unitair

---
+++ DistributeurCafe.Tests/OrderDrinkServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using DistributeurCafe.Model;
+using DistributeurCafe.Services;
+using System.Globalization;
+using Xunit;
+
+
+
+public class OrderDrinkServiceTests
+{
+    [Fact]
+    public void CalculatePrice_ShouldCalculateCorrectly()
+    {
+        // Arrange
+        var orderService = new OrderDrinkService();
+        // Utilisez une recette existante dans le dictionnaire pour le test
+        var expressoRecipe = RecipeDictionary.Recipes["Expresso"];
+
+        // Act
+        var expressoresult = orderService.CalculatePrice(expressoRecipe);
+
+      
+        // Assert
+        Assert.Equal(1.56, expressoresult, 2);
+    }
+
+    [Fact]
+    public void GetFormattedMenu_ShouldReturnCorrectMenu()
+    {
+        // Arrange
+        var orderService = new OrderDrinkService();
+
+        // Act
+        var result = orderService.GetMenu();
+
+        // Assert
+        Assert.Contains("Expresso", result);
+        Assert.Contains("Allongé", result);
+    }
+
+    [Fact]
+    public void CalculatePrice_ShouldReturnZero_ForEmptyRecipe()
+    {
+        var orderService = new OrderDrinkService();
+        var emptyRecipe = new Recipe("EmptyRecipe", new List<Ingredient>());
+
+        var result = orderService.CalculatePrice(emptyRecipe);
+
+        Assert.Equal(0.0, result, 2);
+    }
+
+
+}
